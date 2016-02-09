af8de037a31f5ef82cd05137cba3d36094851f03
msmichellegar <msmichellegar@gmail.com>
2016-02-09 15:34:53 +0000

    adds links to related items

--- public/js/script.js
+++ public/js/script.js
@@ -31,7 +31,7 @@ var flipbook = {
         var topData = [];
         var bottomData = [];
 
-        // assign each page to top or bottom array
+        // assign each flipbook page data to top or bottom array
         data.forEach(function(page) {
             if (page.section === "top") {
                 topData.push(page);
@@ -47,6 +47,7 @@ var flipbook = {
 
     initialise: function initialiseFlipBook(topData, bottomData) {
         var relatedProducts = [];
+        var relatedProductLinks = [];
 
         // insert first pages of flipbook
         $("#top-image").attr("src", "/static/public/images/" + topData[0].image).addClass("0");
@@ -55,13 +56,16 @@ var flipbook = {
         // create array of related products to initialise with
         relatedProducts.push(topData[0]["related-image-1"], topData[0]["related-image-2"], topData[0]["related-image-3"], bottomData[0]["related-image-1"], bottomData[0]["related-image-2"], bottomData[0]["related-image-3"]);
 
+        // create array of related links to initialise with
+        relatedProductLinks.push(topData[0]["related-url-1"], topData[0]["related-url-2"], topData[0]["related-url-3"], bottomData[0]["related-url-1"], bottomData[0]["related-url-2"], bottomData[0]["related-url-3"]);
+
         for (var i=0; i < relatedProducts.length; i++) {
 
             // assign different class to top and bottom products
             var productClass = i < 3 ? "top-product" : "bottom-product";
 
             // append related products to collection
-            $(".collection").append("<div class='collection--item " + productClass +"'><img src='/static/public/images/" + relatedProducts[i] + "'/></div>");
+            $(".collection").append("<a href='" + relatedProductLinks[i] + "'><div class='collection--item " + productClass +"'><img src='/static/public/images/" + relatedProducts[i] + "'/><div class='view-button' href='" + relatedProductLinks[i] + "'>Shop on lyst&nbsp;&nbsp;&nbsp;></div></div></a>");
         }
 
     },
@@ -92,16 +96,20 @@ var flipbook = {
     flip: function flip (direction, section, data) {
         var pageImages = [];
         var relatedProducts = [];
+        var relatedProductLinks = [];
+
         var currentIndex = parseInt($("#" + section + "-image").attr("class"));
         var nextIndex;
 
-        // push images to relevant arrays
+        // push images and links to relevant arrays
         data.forEach(function(page) {
             pageImages.push(page.image);
             relatedProducts.push([page["related-image-1"], page["related-image-2"], page["related-image-3"]]);
+            relatedProductLinks.push([page["related-url-1"], page["related-url-2"], page["related-url-3"]]);
         });
 
-        // determining index of next flipbook page
+
+        // determine index of next flipbook page
         if (direction === "left") {
             if (currentIndex === 0) {
                 nextIndex = pageImages.length - 1;
@@ -119,13 +127,19 @@ var flipbook = {
         // update page image
         $("#" + section + "-image").attr("src", "/static/public/images/" + pageImages[nextIndex]).removeClass().addClass(nextIndex.toString());
 
+        flipbook.insertRelatedProducts(relatedProducts[nextIndex], relatedProductLinks[nextIndex], section);
+
+    },
+
+    insertRelatedProducts: function insertRelatedProducts(images, links, section) {
+        var productClass = section === "top" ? "top-product" : "bottom-product";
+
         // remove current related products
         $("." + section + "-product").remove();
 
         // insert new related products
         for (var i=0; i < 3; i++) {
-            var productClass = section === "top" ? "top-product" : "bottom-product";
-            var content = "<div class='collection--item " + productClass + "'><img src='/static/public/images/" + relatedProducts[nextIndex][i] + "'/></div>";
+            var content = "<a href='" + links[i] + "'><div class='collection--item " + productClass + "'><img src='/static/public/images/" + images[i] + "'/><div class='view-button' href='" + links[i] + "'>Shop on lyst&nbsp;&nbsp;&nbsp;></div></div></a>";
 
             if (section === "top") {
                 $(".collection").prepend(content);
