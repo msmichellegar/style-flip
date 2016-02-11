b0efd0783e44675fb9cddee4a91760842fdbafdf
msmichellegar <msmichellegar@gmail.com>
2016-02-11 12:37:02 +0000

    adds travis for coverage report

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var Hapi = require('hapi');
 var Inert = require('inert');
+var routes = require('./lib/routes.js');
+
 var server = new Hapi.Server();
 
 server.register(Inert, function () {
     server.connection({
         port: process.env.PORT || 8080
     });
-    server.route(require('./lib/routes.js'));
+    server.route(routes);
 });
 
 server.start(function() {
